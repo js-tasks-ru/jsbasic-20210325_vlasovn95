2d2027a7bf23468b8254e2bb6a47097bfcded212
Nikita Vlasov <vlasovn95@yandex.ru>
2021-04-13 14:29:34 +0300

    задача 1-1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0 || n == 1) {
+    return 1
+  }
+
+  let result = n
+
+  for (let x = n - 1; x > 0; x--) {
+    result *= x
+  }
+
+  return result
 }
